4e56ccea19a7c32aebcedbe630e880d865cefae3
Dennis Bailey <bailey.dennis@gmail.com>
2016-03-16 08:12:08 -0600

    Cleaned up instructor-queries.js. Added comments and moved commented out code to a scratch file. Added the scratch file to .gitignore

--- src/server/queries/instructor_queries.js
+++ src/server/queries/instructor_queries.js
@@ -1,49 +1,9 @@
 var knex = require("../../../db/knex");
 
-// *** Helper Functions *** //
-// function Students() {
-//   return knex('users').select('first as fName', 'last as lName', 'id as student_id').as('users');
-// }
-// 
-// function allAssessmentRatings() {
-//       return knex('student_feedback').select('type').avg('rating').groupBy('type')
-//       .innerJoin('curricula', 'curricula.id', 'student_feedback.curriculum_id')
-//       .innerJoin('types', 'curricula.type_id', 'types.id');
-// }
-
-// function findMedian(id){
-// var counts = knex('student_feedback').select('curricula.id').count('student_feedback.id')
-//         .innerJoin('curricula', 'curricula.id', 'student_feedback.curriculum_id')
-//         .where('curricula.type_id', id)
-//         .groupBy('curricula.id', 'curricula.assignmentDt');
-//         
-//         var medians = [];
-//         console.log('counts ',counts);
-//         for (var i = 0; i < counts.length; i++) { 
-//             console.log('hello');
-//             var result = {id: i.id}
-//             
-//             var row;
-//             
-//             if (i.count % 2 === 0) {
-//                 row = i.count / 2;
-//             } else {
-//                 row = Math.floor(i.count / 2);
-//             }
-//             
-//             result.median = knex('student_feedback').select('student_feedback.rating').limit(1).offset(count)
-//                             .where('student_feedback.curriculum_id', i.id)
-//                             .orderBy('student_feedback.rating', 'asc');
-//                             
-//             medians.push(result);
-//           
-//         }
-//         console.log(medians);
-//         return medians;
-// };
 
 module.exports = {
-  
+    
+    // Find all completed assessments for all students
     allAssessments : function() {
       
         return knex('users')
@@ -53,9 +13,9 @@ module.exports = {
         
     },
     
+    // Find the mean rating for each element of the curriculum for this type
     avgTypeAssessments : function(type) {
         
-        // Find the mean rating for each element of the curriculum for this type
         return knex('student_feedback')
         .select('curricula.id','curricula.type_id', 'curricula.title', 'curricula.assignmentDt').avg('student_feedback.rating')
         .innerJoin('curricula', 'curricula.id', 'student_feedback.curriculum_id')
@@ -65,9 +25,9 @@ module.exports = {
         
     },
     
+     // Find the mean rating for each element of the curriculum for this type
     allTypeAssessments : function(type, id) {
         
-        // Find the mean rating for each element of the curriculum for this type
         return knex('users')
         .innerJoin('student_feedback', 'student_feedback.student_id', 'users.id')
         .innerJoin('curricula', 'curricula.id', 'student_feedback.curriculum_id')
@@ -77,6 +37,7 @@ module.exports = {
         
     },
     
+     // Find the mean rating for each learning experience type
     avgAssessments : function() {
         
       return knex('student_feedback').select('type', 'type_id').avg('rating').groupBy('type_id', 'type')
