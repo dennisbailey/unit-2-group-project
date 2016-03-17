1907563064f4c3a8c2883beff41d3f56385c6fdc
Lina Sachuk <sachuklina@gmail.com>
2016-03-17 11:26:01 -0600

    rename admin route and added edit and delete links for admin page

--- src/server/queries/admin_queries.js
+++ src/server/queries/admin_queries.js
@@ -27,5 +27,12 @@ module.exports = {
         
         return knex('topics');
       
-    }
+    },
+
+    allAssignments : function(){
+    return knex('curricula')
+    .innerJoin('types', 'types.id', 'curricula.type_id')
+    .leftOuterJoin('instructors', 'instructors.id', 'curricula.instructor_id')
+    .orderBy('assignmentDt', 'desc');
+  }
 };
