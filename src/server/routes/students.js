141497f165a3e6f9cd057ae8a0034d4f5c5e4b15
Dennis Bailey <bailey.dennis@gmail.com>
2016-03-16 20:11:09 -0600

    Cleaned up students.js and added more comments

--- src/server/routes/students.js
+++ src/server/routes/students.js
@@ -3,56 +3,74 @@ var router = express.Router();
 var queries = require('../queries/students_queries');
 
 
-
-//helper function section 
+// *** Helper Functions *** //
+// Return the user's name for use in Swig
 function userName(user){
-  var name = '';
-  if(user){
-    name = user.first;
-  }
-  return name;
+    var name = '';
+    if(user){
+      name = user.first;
+    }
+    return name;
 }
 
+// Create an array of objects to pass to knex to insert feedback into the table student_feedback
 function createInsert(object){
-
-   var body = object;
-   var insert =[];
-   var keys = Object.keys(body);
-   var today = new Date().toJSON().slice(0,10);
-   
-   for(i=0; i < keys.length; i++){
-      if(body[keys[i]] !== ''){
-        var object= {};
-        object.student_id = req.user.id;
-        object.curriculum_id = keys[i];
-        object.rating = body[keys[i]];
-        object.feedbackDt =  today;
-
-        insert.push(object);
-      }
-     }
-   return insert;
+    
+    // This function will take an object (req.body) with the key value pairs of curriculum ID and rating
+    // Find all the curriculum Ids with Object.keys() so we can loop through them
+    var body = object;
+    var keys = Object.keys(body);
+    
+    // Declare an empty array that will hold all of the objects that knex will use to create new rows
+    var insert =[];
+    
+    // Declare a variable today that captures today's date to be used for the feedback date
+    var today = new Date().toJSON().slice(0,10);
+    
+    // Loop through the keys to create an object to pass to knex
+    for(i=0; i < keys.length; i++){
+        
+        // If the curriculum ID in the keys has a correcponding rating create an object
+        // If there is no rating, ignore that curriculum ID 
+        if(body[keys[i]] !== ''){
+          
+          var object= {};
+          
+          // Create the key value pairs that correspond to the columns in student_feedback and their values
+          object.student_id = req.user.id;
+          object.curriculum_id = keys[i];
+          object.rating = body[keys[i]];
+          object.feedbackDt =  today;
+          
+          // Push the object to the array insert before checking the next curriculum ID in keys
+          insert.push(object);
+        
+        }
+    }
+    
+    // Return an array of objects that contain all the information we want to insert into student_feedback
+    return insert;
  
  }
 
-
+// The home route for /students. This will show students all the assessments that have not yet been rated
 router.get('/', function(req, res, next) {
 
-   queries.allUnratedForOneStudent(req.user.id)
-    .then(function(result){
-      console.log(result)
-        res.render('students', { title: 'students', data: result, name: userName(req.user)});
-      })
+    queries.allUnratedForOneStudent(req.user.id)
     
-    .catch(function(error){
-      return error;
-    });
+    .then(function(result){
+        res.render('students', { title: 'students', 
+                                 data: result, 
+                                 name: userName(req.user) });
+    })
+    
+    .catch(function(error) { return error; });
 
 });
 
 
-
-
+// This GET route shows a student all of the learning experiences and attaches a rating if present
+// If there's no rating, the learning experience will offer them a link back to the assesments page
 router.get('/all', function(req, res, next){
     
     queries.allAssessmentsForOneStudent(req.user.id)
@@ -60,64 +78,46 @@ router.get('/all', function(req, res, next){
     .then( function (result) { 
      
         res.render('studentsAll', { title: 'Students',
-                                    data: result , name: userName(req.user)});
+                                    data: result , name: userName(req.user) });
     })
     
-    .catch( function ( error ){
-     
-      return error; });
+    .catch(function (error) { return error; });
   
 });
 
 
 
-
+// This POST route creates an array of objects to pass to knex and insert into student_feedback
 router.post('/addrating', function(req, res, next){
-       
-   var ratedRatings = req.body;
-   var insertbody = createInsert(ratedRatings);
-
-   queries.insertFeedback(insertbody)
-
-   .then( function (result) { 
+    
+    // Pass req.body to createInsert()   
+    var ratings = req.body;
+    var insertbody = createInsert(ratings);
+    
+    // Pass the array of objects to knex
+    queries.insertFeedback(insertbody)
+    
+    .then( function (result) { 
         res.redirect('/students');
     })
     
-    .catch( function ( error )
-     {
-      return error; });
+    .catch(function (error) { return error; });
 
 });
 
 
-router.get('/all/:id', function(req, res, next){
-    
-    queries.allAssessmentsForOneStudent(req.user.id)
-    
-    .then( function (result) { 
-        res.render('studentsAll', { title: 'Students',
-                                    data: result , name: userName(req.user)});
-    })
-    
-    .catch( function ( error )
-     {console.log(error); 
-      return error; });
-  
-});
-
-
-
-
+// This GET route returns all assessments a student has completed for a specific type of learning experience
 router.get('/:typeid', function(req, res, next){
 
     queries.allAssessmentsByTypeForOneStudent(req.params.typeid)
     
     .then( function (result) { 
         res.render('students-type', { title: 'students',
-                                    data: result, name: userName(req.user) });
+                                      data: result, 
+                                      name: userName(req.user) });
     })
     
-    .catch( function ( error ) { return error; });
+    .catch(function (error) { return error; });
   
 });
 
