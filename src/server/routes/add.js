ebaf25dbb37333c2dcc5d93a14db859b2995f2e7
Dennis Bailey <bailey.dennis@gmail.com>
2016-03-17 10:06:16 -0600

    modified the get route for add/currriculum to query the databse and return values to pass to swig to populate the page

--- src/server/routes/add.js
+++ src/server/routes/add.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var index_queries = require('../queries/index_queries');
+var queries = require('../queries/add_queries');
 var passport = require('../lib/auth');
 var helpers = require('../lib/helpers');
 
@@ -8,10 +8,48 @@ var helpers = require('../lib/helpers');
 // *** Routes *** //
 // Show the add to curriculum route
 router.get('/curriculum', function(req, res, next) {
-  
-    res.render('add-curriculum', { title: 'Add To Curriculum' });
+    
+    var promises = [];
+    
+    promises.push(queries.allInstructors());
+    
+    promises.push(queries.allTypes());
+    
+    promises.push(queries.allTopics());
+    
+    return Promise.all(promises)
+    
+    .then( function (result) {  
 
+        res.render('add-curriculum', { title: 'Add To Curriculum',
+                                       instructors: result[0],
+                                       types: result[1],
+                                       topics: result[2] });
+      
+    })
+    
+    .catch( function ( result ) { console.log(result); return result; })
+    
 });
+
+// POST to add to the curriculum Registration and check for a unique email
+router.post('/curriculum', function(req, res, next) {
+    
+    queries.addLearningExperience(req.body)
+    
+    .then(function (result) {  
+        res.render('add-curriculum', { title: 'Add To Curriculum',
+                                       message: 'New Learning Experience Added to the Curriculum' });
+      
+    })
+    
+    .catch( function ( result ) { return result; })
+    
+});
+    
+    
+
+
 
 
 module.exports = router;
